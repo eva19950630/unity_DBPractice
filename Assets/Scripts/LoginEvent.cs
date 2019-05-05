1be231828b2c8e8f292ad7ad684b0676f6b8ece6
eva19950630 <fun910347@gmail.com>
2019-05-05 19:26:36 +0800

    first commit

---
+++ Assets/Scripts/LoginEvent.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class LoginEvent : MonoBehaviour {
+	public Text Text_account, Text_password;
+	public GameObject Panel_hint;
+	public static string account;
+	private bool isLogin;
+
+	// Use this for initialization
+	void Start () {
+		isLogin = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void clickRegister() {
+		string newAccount = Text_account.text;
+		string newPassword = Text_password.text;
+		// 如果使用者有欄位沒有輸入
+		if (newAccount == "" || newPassword == "") {
+			Panel_hint.SetActive(true);
+			Panel_hint.transform.GetChild(0).GetComponent<Text>().text = "有欄位沒有填寫喔！";
+		} else {
+			StartCoroutine(userRegister(newAccount, newPassword));
+		}
+	}
+
+	IEnumerator userRegister(string newAccount, string newPassword) {
+		string URL = "http://localhost/unity_190519/api/UserController.php";
+		
+		WWWForm form = new WWWForm();
+		form.AddField("newAccount", newAccount);
+		form.AddField("newPassword", newPassword);
+
+		using(UnityWebRequest www = UnityWebRequest.Post(URL, form)){
+            yield return www.SendWebRequest();
+
+            if(www.isNetworkError || www.isHttpError) {
+            	print(www.error);
+        	} else {
+        		print(www.downloadHandler.text);
+        		Panel_hint.SetActive(true);
+        		if (www.downloadHandler.text == "register successfully!") {
+        			Panel_hint.transform.GetChild(0).GetComponent<Text>().text = "註冊成功！";
+        			account = newAccount;
+        			isLogin = true;
+        		} else if (www.downloadHandler.text == "account existed!") {
+        			Panel_hint.transform.GetChild(0).GetComponent<Text>().text = "此帳號已被註冊！";
+        		}
+        	}
+        }
+	}
+
+	public void clickLogin() {
+		string userAccount = Text_account.text;
+		string userPassword = Text_password.text;
+		// 如果使用者有欄位沒有輸入
+		if (userAccount == "" || userPassword == "") {
+			Panel_hint.SetActive(true);
+			Panel_hint.transform.GetChild(0).GetComponent<Text>().text = "有欄位沒有填寫喔！";
+		} else {
+			StartCoroutine(userLogin(userAccount, userPassword));
+		}
+	}
+
+	IEnumerator userLogin(string userAccount, string userPassword) {
+		string URL = "http://localhost/unity_190519/api/UserController.php";
+
+		WWWForm form = new WWWForm();
+		form.AddField("userAccount", userAccount);
+		form.AddField("userPassword", userPassword);
+
+		using(UnityWebRequest www = UnityWebRequest.Post(URL, form)){
+            yield return www.SendWebRequest();
+            
+            if(www.isNetworkError || www.isHttpError) {
+            	print(www.error);
+        	} else {
+        		print(www.downloadHandler.text);
+        		Panel_hint.SetActive(true);
+        		if (www.downloadHandler.text == "login successfully!") {
+        			Panel_hint.transform.GetChild(0).GetComponent<Text>().text = "登入成功！";
+        			account = userAccount;
+        			isLogin = true;
+        		} else if (www.downloadHandler.text == "wrong password!") {
+        			Panel_hint.transform.GetChild(0).GetComponent<Text>().text = "密碼錯誤！";
+        		} else if (www.downloadHandler.text == "No register!") {
+        			Panel_hint.transform.GetChild(0).GetComponent<Text>().text = "尚未註冊！";
+        		}
+        	}
+        }
+	}
+
+	public void clickConfirm() {
+		if (isLogin)
+			SceneManager.LoadScene("menu");
+	}
+}
---
+++ Assets/Scripts/UserRecord.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+// using UnityEngine.SceneManagement;
+// using UnityEngine.EventSystems;
+
+public class UserRecord : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject.Find("Text_account").GetComponent<Text>().text = LoginEvent.account;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// public void clickBtn() {
+	// 	string account, action, target, scene, behaviorLog;
+	// 	account = LoginEvent.account;
+	// 	action = "click";
+	// 	target = EventSystem.current.currentSelectedGameObject.name;
+	// 	scene = SceneManager.GetActiveScene().name;
+	// 	behaviorLog = account + "#" + action + "#" + target + "#" + scene;
+
+	// 	StartCoroutine(uploadBehavior(behaviorLog));
+	// }
+
+	// IEnumerator uploadBehavior(string behaviorLog) {
+	// 	string URL = "http://163.21.245.192/PigSaviorAPP/teach_unity/behaviorUpload.php";
+	// 	WWWForm form = new WWWForm();
+	// 	Dictionary<string, string> data = new Dictionary<string, string>();
+	// 	data.Add("log", behaviorLog);
+	// 	foreach (KeyValuePair<string, string> post in data) {
+	// 		form.AddField(post.Key, post.Value);
+	// 	}
+	// 	WWW www = new WWW(URL, form);
+	// 	yield return www;
+	// 	print(www.text);
+	// }
+}
